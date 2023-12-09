ede7c437e7d42646067ad2867628925616cda93c
Mohamed_Bedier <113776299+MohamedBedier@users.noreply.github.com>
2023-12-09 12:44:29 +0200

    new projects Traffic light using stm32f103c8t6

---
+++ Traffic_Light_f103/Inc/Systick_private.h
@@ -0,0 +1,36 @@
+/*************************************************************/
+/*************************************************************/
+/**                                                         **/
+/** LayerArchitect: MCAL                                    **/
+/** File Name : Systick_private.h                           **/
+/** Auther    : MOHAMED BEDIER MOHAMED                      **/
+/** Verision :  1.00                                        **/
+/**                                                         **/
+/*************************************************************/
+/*************************************************************/
+
+
+#ifndef SYSTICK_PRIVATE_H_
+#define SYSTICK_PRIVATE_H_
+
+
+#define  AHB_CLK_DIVIDED_bY_8 0u
+#define AHB_CLK  1u
+
+#define CONVERT_FROM_US_TO_MS  1000u
+
+#define MAX_RELOAD_VALUE   16777215U
+
+#define  FLAG_NOT_RAISED     0u
+
+#define RESET_RELOAD_VALUE  0u
+#define SYS_FREQ_8MHZ  8u
+
+#define SYSTICK_MASK    0xFFFFFF
+
+
+#define AHB_TICK_TIME     0.0625f
+#define AHB_BY8_TICK_TIME 0.5f
+
+
+#endif /* SYSTICK_PRIVATE_H_ */
